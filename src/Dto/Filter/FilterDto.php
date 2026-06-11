c0ddeb809a9a8e558f8a21ad0228c6b83ce54bb4
aleksey <example@example.com>
2026-06-11 13:07:54 +0400

    refactor(src/Dto/Filter/FilterDto.php): Add iterator implementation and implement Countable interface for FilterDto

--- src/Dto/Filter/FilterDto.php
+++ src/Dto/Filter/FilterDto.php
@@ -2,7 +2,12 @@
 
 namespace Wertelko\EasyadminMapperBundle\Dto\Filter;
 
-class FilterDto
+use ArrayIterator;
+use Countable;
+use IteratorAggregate;
+use Traversable;
+
+class FilterDto implements IteratorAggregate, Countable
 {
     public function __construct(
         private readonly array $filters = [],
@@ -13,5 +18,15 @@ class FilterDto
     public function get(string $name, $default = null): mixed
     {
         return $this->filters[$name] ?? $default;
+    }
+
+    public function getIterator(): Traversable
+    {
+        return new ArrayIterator($this->filters);
+    }
+
+    public function count(): int
+    {
+        return count($this->filters);
     }
 }
